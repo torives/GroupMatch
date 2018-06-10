50f3b36876e9816088e80a87d2da6d4ef1afa64e
Victor Yves Crispim <victoryvescrispim@gmail.com>
2018-06-10 16:47:40 -0300

    Exibindo mais de um fragment

--- app/src/main/java/br/com/yves/groupmatch/MainActivity.kt
+++ app/src/main/java/br/com/yves/groupmatch/MainActivity.kt
@@ -3,28 +3,53 @@ package br.com.yves.groupmatch
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
-import android.util.Log
 import android.view.MenuItem
 import br.com.yves.groupmatch.ui.main.MainFragment
+import br.com.yves.groupmatch.ui.main.MainView
 import kotlinx.android.synthetic.main.main_activity.*
 
-class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity(), MainView, BottomNavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction().replace(R.id.container, MainFragment.newInstance()).commitNow()
+            displayCalendarScene()
         }
 
         this.bottomNavigationView.setOnNavigationItemSelectedListener(this)
     }
 
 
+    /*
+        MainView Implementation
+     */
+
+    override fun displayCalendarScene() = supportFragmentManager
+            .beginTransaction()
+            .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+            .replace(R.id.container, MainFragment.newInstance()).commitNow()
+
+    override fun displaySearchScene() = supportFragmentManager
+            .beginTransaction()
+            .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+            .replace(R.id.container, SearchDevicesFragment.newInstance()).commitNow()
+
+    override fun displaySettingsScene() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    /*
+       BottomNavigationView.OnNavigationItemSelectedListener Implementation
+    */
+
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        Log.d(this.getString(R.string.app_log_id), "Hello")
+
+        when (item.itemId) {
+            R.id.calendarTabIcon -> this.displayCalendarScene()
+            R.id.searchTabIcon -> this.displaySearchScene()
+        }
         return true
     }
 }
--- app/src/main/java/br/com/yves/groupmatch/ui/main/MainView.kt
+++ app/src/main/java/br/com/yves/groupmatch/ui/main/MainView.kt
@@ -1,5 +1,7 @@
 package br.com.yves.groupmatch.ui.main
 
 interface MainView {
-
+    fun displayCalendarScene()
+    fun displaySearchScene()
+    fun displaySettingsScene()
 }
