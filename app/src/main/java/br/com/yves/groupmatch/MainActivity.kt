54285eae10e98b3f4425f689c13bc1f1af0c5a4c
Victor Yves Crispim <victoryvescrispim@gmail.com>
2018-06-09 23:31:51 -0300

    Implementando BottomNavigationView.OnNavigationItemSelectedListener

--- app/src/main/java/br/com/yves/groupmatch/MainActivity.kt
+++ app/src/main/java/br/com/yves/groupmatch/MainActivity.kt
@@ -2,16 +2,29 @@ package br.com.yves.groupmatch
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.util.Log
+import android.view.MenuItem
 import br.com.yves.groupmatch.ui.main.MainFragment
+import kotlinx.android.synthetic.main.main_activity.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction().replace(R.id.container, MainFragment.newInstance()).commitNow()
         }
+
+        this.bottomNavigationView.setOnNavigationItemSelectedListener(this)
     }
 
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        Log.d(this.getString(R.string.app_log_id), "Hello")
+        return true
+    }
 }
