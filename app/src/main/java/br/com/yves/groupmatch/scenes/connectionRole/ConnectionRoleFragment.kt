41e26c71cfaed9b732c25e92801a687c6d8b984b
Victor Yves Crispim <victoryvescrispim@gmail.com>
2018-06-19 23:10:21 -0300

    Adicionanado os botões no ConnectionRoleFragment

--- app/src/main/java/br/com/yves/groupmatch/scenes/connectionRole/ConnectionRoleFragment.kt
+++ app/src/main/java/br/com/yves/groupmatch/scenes/connectionRole/ConnectionRoleFragment.kt
@@ -6,10 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import br.com.yves.groupmatch.R
+import kotlinx.android.synthetic.main.fragment_connection_role.*
+import org.jetbrains.anko.support.v4.toast
 
 class ConnectionRoleFragment: Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_connection_role, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupButtonsOnClickListener()
+    }
+
+    private fun setupButtonsOnClickListener() {
+        serverButton.setOnClickListener { toast("server") }
+        clientButton.setOnClickListener { toast("client") }
     }
 }
