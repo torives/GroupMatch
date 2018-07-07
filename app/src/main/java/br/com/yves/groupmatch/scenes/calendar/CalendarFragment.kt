7bcce9fd580a10c7496eab0b3a9583fda094d15b
Victor Yves Crispim <vsilva@horizonte.tv.br>
2018-07-07 15:17:19 -0300

    Adicionando um listener nas células

--- app/src/main/java/br/com/yves/groupmatch/scenes/calendar/CalendarFragment.kt
+++ app/src/main/java/br/com/yves/groupmatch/scenes/calendar/CalendarFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,7 +15,7 @@ import androidx.navigation.fragment.NavHostFragment
 import br.com.yves.groupmatch.R
 import kotlinx.android.synthetic.main.fragment_calendar.*
 
-class CalendarFragment: NavHostFragment() {
+class CalendarFragment: NavHostFragment(), DaysAdapter.ItemClickListener {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_calendar, container, false)
@@ -24,15 +25,20 @@ class CalendarFragment: NavHostFragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val adapter = DaysAdapter(context!!, arrayOf("treta", "tretinha", "tretosa"))
+        adapter.listener = this
         daysRecyclerView.layoutManager = GridLayoutManager(context!!, 3)
         daysRecyclerView.adapter = adapter
+    }
+
+    override fun onItemClick(view: View, position: Int) {
+        Log.d("treta", view.findViewById<TextView>(R.id.info_text).text.toString())
     }
 
 }
 
 class DaysAdapter(context: Context, private var data: Array<String>): RecyclerView.Adapter<DaysAdapter.DayViewHolder>() {
     private val layoutInflater: LayoutInflater = LayoutInflater.from(context)
-    var listener: AdapterView.OnItemClickListener? = null
+    var listener: ItemClickListener? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DaysAdapter.DayViewHolder {
         val view = layoutInflater.inflate(R.layout.calendar_day_recyclerview_item, parent, false)
@@ -47,10 +53,14 @@ class DaysAdapter(context: Context, private var data: Array<String>): RecyclerVi
         holder.textView.text = data[position]
     }
 
+    interface ItemClickListener {
+        fun onItemClick(view: View, position: Int)
+    }
+
     inner class DayViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val textView: TextView = view.findViewById<TextView>(R.id.info_text)
+        val textView: TextView = view.findViewById(R.id.info_text)
         init {
-           // view.setOnClickListener { listener?. }
+            view.setOnClickListener { listener?.onItemClick(view, adapterPosition) }
         }
     }
 }
