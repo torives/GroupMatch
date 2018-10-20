cb972324b54fda536fe8c3fc78f2d5ea1f704c36
Victor Yves Crispim <rotcivyves@gmail.com>
2018-10-20 17:33:15 -0300

    [WIP] Configurando o Room

---
+++ data/src/main/java/br/com/yves/groupmatch/data/db/RoomDB.kt
@@ -0,0 +1,85 @@
+package br.com.yves.groupmatch.data.db
+
+import android.content.Context
+import android.service.autofill.FillEventHistory
+import androidx.room.*
+import br.com.yves.groupmatch.data.db.EventRoom.Companion.COLUMN_ID
+import org.threeten.bp.LocalDateTime
+import br.com.yves.groupmatch.data.db.EventRoom.Companion.TABLE_NAME
+import br.com.yves.groupmatch.domain.loadEvents.Event
+import com.yves.groupmatch.data.R
+import org.threeten.bp.format.DateTimeFormatter
+
+
+@Database(
+        entities = [EventRoom::class],
+        version = 1)
+@TypeConverters(RoomTypeConverter::class)
+abstract class RoomDB : RoomDatabase() {
+
+    abstract fun eventDAO(): EventDAO
+
+    companion object {
+        private var INSTANCE: RoomDB? = null
+
+        fun getInstance(): RoomDB {
+            return INSTANCE ?: throw IllegalStateException("Must initialize RoomDB on Application")
+        }
+
+        fun init(applicationContext: Context) {
+            INSTANCE = Room.databaseBuilder(
+                    applicationContext,
+                    RoomDB::class.java,
+                    applicationContext.getString(R.string.database_name)
+            ).build()
+        }
+    }
+}
+
+
+@Dao
+interface EventDAO {
+    //region Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertOrReplace(event: EventRoom)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertOrReplace(events: List<EventRoom>)
+    //endregion
+
+    //region Delete
+    @Delete
+    fun delete(event: EventRoom)
+
+    @Delete
+    fun delete(events: List<EventRoom>)
+    //endregion
+}
+
+@Entity(tableName = TABLE_NAME,
+        indices = [Index(COLUMN_ID, unique = true)])
+data class EventRoom(
+        @ColumnInfo(name = COLUMN_ID) @PrimaryKey val id: Int,
+        val date: LocalDateTime
+) {
+    companion object {
+        const val TABLE_NAME = "events"
+        const val COLUMN_ID = "id"
+    }
+}
+
+class RoomTypeConverter {
+    private val formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME
+
+    @TypeConverter
+    fun fromTimestamp(value: String?): LocalDateTime? {
+        return value?.let {
+            formatter.parse(it, LocalDateTime::from)
+        }
+    }
+
+    @TypeConverter
+    fun dateToTimestamp(date: LocalDateTime?): String? {
+        return date?.format(formatter)
+    }
+}
--- presentation/src/main/java/br/com/yves/groupmatch/presentation/Application.kt
+++ presentation/src/main/java/br/com/yves/groupmatch/presentation/Application.kt
@@ -1,12 +1,15 @@
 package br.com.yves.groupmatch.presentation
 
 import android.app.Application
+import br.com.yves.groupmatch.data.db.RoomDB
 import com.jakewharton.threetenabp.AndroidThreeTen
 
 class Application : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
         AndroidThreeTen.init(this)
+        RoomDB.init(this)
     }
 }
