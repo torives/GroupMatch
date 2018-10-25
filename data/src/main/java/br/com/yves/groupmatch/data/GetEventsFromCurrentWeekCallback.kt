d7ca4dd76c67f4ece7bec035889328adc6f3866f
Victor Yves Crispim <rotcivyves@gmail.com>
2018-10-25 00:18:19 -0300

    Removendo classes não utilizadas

--- data/src/main/java/br/com/yves/groupmatch/data/GetEventsFromCurrentWeekCallback.kt
+++
@@ -1,7 +0,0 @@
-package br.com.yves.groupmatch.data
-
-import br.com.yves.groupmatch.domain.loadEvents.Event
-
-interface GetEventsFromCurrentWeekCallback {
-    fun onGetEventsFromCurrentWeekSuccess(events: List<Event>)
-}
