3873a5750db61add8968e56c2b032dab74f7ec5e
Victor Yves Crispim <rotcivyves@gmail.com>
2018-11-04 17:59:23 -0200

    Atualizando TimeSlotRoom
    Utilizando date como chave primária

--- data/src/main/java/br/com/yves/groupmatch/data/db/timeSlot/TimeSlotRoom.kt
+++ data/src/main/java/br/com/yves/groupmatch/data/db/timeSlot/TimeSlotRoom.kt
@@ -4,22 +4,20 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
-import br.com.yves.groupmatch.data.db.timeSlot.TimeSlotRoom.Companion.COLUMN_ID
+import br.com.yves.groupmatch.data.db.timeSlot.TimeSlotRoom.Companion.COLUMN_DATE
 import br.com.yves.groupmatch.data.db.timeSlot.TimeSlotRoom.Companion.TABLE_NAME
 import org.threeten.bp.LocalDateTime
 
 @Entity(
 	tableName = TABLE_NAME,
-	indices = [Index(COLUMN_ID, unique = true)]
+	indices = [Index(COLUMN_DATE, unique = true)]
 )
 open class TimeSlotRoom(
-	@ColumnInfo(name = COLUMN_ID) @PrimaryKey(autoGenerate = true) val id: Int,
-	@ColumnInfo(name = COLUMN_DATE) val date: LocalDateTime,
+	@ColumnInfo(name = COLUMN_DATE) @PrimaryKey val date: LocalDateTime,
 	@ColumnInfo(name = COLUMN_IS_BUSY) val isBusy: Boolean
 ) {
 	companion object {
 		const val TABLE_NAME = "time_slot"
-		const val COLUMN_ID = "id"
 		const val COLUMN_DATE = "date"
 		const val COLUMN_IS_BUSY = "is_busy"
 	}
