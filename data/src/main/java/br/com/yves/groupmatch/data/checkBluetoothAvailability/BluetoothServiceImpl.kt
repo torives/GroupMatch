e94917d02c40413093f349b859e701be8ce3f955
Victor Yves Crispim <rotcivyves@gmail.com>
2018-11-13 02:15:14 -0200

    BluetoothClient
    - Alterando BluetoothServiceImpl para manter uma referência ao contexto da aplicação

--- data/src/main/java/br/com/yves/groupmatch/data/checkBluetoothAvailability/BluetoothServiceImpl.kt
+++ data/src/main/java/br/com/yves/groupmatch/data/checkBluetoothAvailability/BluetoothServiceImpl.kt
@@ -1,24 +1,29 @@
 package br.com.yves.groupmatch.data.checkBluetoothAvailability
 
+import android.Manifest
 import android.bluetooth.BluetoothManager
 import android.content.Context
 import android.content.pm.PackageManager
-import br.com.yves.groupmatch.domain.checkBluetoothAvailability.BluetoothService
+import androidx.core.content.PermissionChecker.checkSelfPermission
+import br.com.yves.groupmatch.domain.BluetoothService
 
 class BluetoothServiceImpl(context: Context) : BluetoothService {
 
-	private val bluetoothManager =
-		context.getSystemService(Context.BLUETOOTH_SERVICE) as? BluetoothManager
+	private val context = context.applicationContext
 
-	private val isBLESupported =
-		context.packageManager?.hasSystemFeature(
-			PackageManager.FEATURE_BLUETOOTH_LE
-		)
+	private val bluetoothManager =
+		this.context.getSystemService(Context.BLUETOOTH_SERVICE) as? BluetoothManager
 
 	override fun isBluetoothTurnedOn() = bluetoothManager?.adapter?.isEnabled == true
 
-	override fun isBLESupported() = isBLESupported == true
+	override fun isBLESupported() = context.packageManager?.hasSystemFeature(
+		PackageManager.FEATURE_BLUETOOTH_LE
+	) == true
 
-	override fun isAdvertisementSupported() = bluetoothManager?.adapter?.isMultipleAdvertisementSupported == true
+	override fun isAdvertisementSupported() =
+		bluetoothManager?.adapter?.isMultipleAdvertisementSupported == true
+
+	override fun isLocationPermissionGranted() =
+		checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
 }
 
