4e221a2387663885f59377c476c06c4ccfcb5c6d
Victor Yves Crispim <rotcivyves@gmail.com>
2018-11-24 18:42:37 -0200

    BluetoothServer
    - Importando BluetoothChatService e suas dependências
    - Iniciando a implementação do BluetoothServerFragment

--- presentation/src/main/java/br/com/yves/groupmatch/presentation/ui/bluetooth/availability/server/BluetoothServerFragment.kt
+++ presentation/src/main/java/br/com/yves/groupmatch/presentation/ui/bluetooth/availability/server/BluetoothServerFragment.kt
@@ -2,20 +2,20 @@ package br.com.yves.groupmatch.presentation.ui.bluetooth.availability.server
 
 
 import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothDevice
-import android.bluetooth.BluetoothManager
-import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import android.os.Looper
-import android.util.Log
+import android.os.Message
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import br.com.yves.groupmatch.R
-import kotlinx.android.synthetic.main.fragment_search_bluetooth_clients.*
-import kotlinx.android.synthetic.main.view_log.view.*
+import br.com.yves.groupmatch.presentation.ui.bluetooth.availability.client.ServerListAdapter
+import kotlinx.android.synthetic.main.fragment_bluetooth_client.*
 import java.util.*
 
 
@@ -24,51 +24,132 @@ val SERVICE_UUID: UUID = UUID.fromString(SERVICE_STRING)
 
 class BluetoothServerFragment : Fragment() {
 
-	private var mBluetoothManager: BluetoothManager? = null //FIXME: Receber por parâmetro?
-	private var mBluetoothAdapter: BluetoothAdapter? = null //FIXME: Receber por parâmetro?
+	private lateinit var bluetoothAdapter: BluetoothAdapter
+	private lateinit var serverAdapter: ServerListAdapter
+	private lateinit var mChatService: BluetoothChatService
+	private lateinit var mOutStringBuffer: StringBuffer
 
-	private val mHandler: Handler = Handler()
-	private val mLogHandler: Handler = Handler(Looper.getMainLooper())
-	private val mDevices = ArrayList<BluetoothDevice>()
-
+	//region Lifecycle
 	override fun onCreateView(
 		inflater: LayoutInflater,
 		container: ViewGroup?,
 		savedInstanceState: Bundle?
 	): View? {
-		return inflater.inflate(R.layout.fragment_search_bluetooth_clients, container, false)
+		return inflater.inflate(R.layout.fragment_bluetooth_client, container, false)
 	}
 
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
 
-		mBluetoothManager =
-				activity?.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager?
-		mBluetoothAdapter = mBluetoothManager?.adapter
+		foundServersList.layoutManager = LinearLayoutManager(context)
+		serverAdapter = ServerListAdapter()
+		foundServersList.adapter = serverAdapter
+		foundServersList.addItemDecoration(
+			DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
+		)
+
+		fab.setOnClickListener {
+			ensureDiscoverable()
+		}
+	}
+
+	override fun onStart() {
+		super.onStart()
+		setupChat()
 	}
 
 	override fun onResume() {
 		super.onResume()
-	}
-
-	// Logging
-
-	fun log(msg: String) {
-		Log.d(TAG, msg)
-		mLogHandler.post {
-			serverLogView.logTextView.append(msg + "\n")
-			serverLogView.logScrollView.post { serverLogView.logScrollView.fullScroll(View.FOCUS_DOWN) }
+		// Only if the state is STATE_NONE, do we know that we haven't started already
+		if (mChatService.state == BluetoothChatService.STATE_NONE) {
+			// Start the Bluetooth chat services
+			mChatService.start()
 		}
 	}
 
-	private fun clearLogs() {
-		mLogHandler.post { serverLogView.logTextView.text = "" }
+	override fun onDestroy() {
+		super.onDestroy()
+		mChatService.stop()
+	}
+	//endregion
+
+	private fun setupChat() {
+		super.onStart()
+
+		// Get the local Bluetooth adapter
+		bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+		// Initialize the BluetoothChatService to perform bluetooth connections
+		mChatService = BluetoothChatService(activity, mHandler)
+		// Initialize the buffer for outgoing messages
+		mOutStringBuffer = StringBuffer("")
+	}
+
+	/**
+	 * Makes this device discoverable.
+	 */
+	private fun ensureDiscoverable() {
+		if (bluetoothAdapter.scanMode != BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
+			val discoverableIntent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE)
+			discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300)
+			startActivity(discoverableIntent)
+		}
+	}
+
+
+	/**
+	 * The Handler that gets information back from the BluetoothChatService
+	 */
+	private val mHandler = object : Handler() {
+		override fun handleMessage(msg: Message) {
+			val activity = activity
+			when (msg.what) {
+				Constants.MESSAGE_STATE_CHANGE -> when (msg.arg1) {
+					BluetoothChatService.STATE_CONNECTED -> {
+//						setStatus(getString(R.string.title_connected_to, mConnectedDeviceName))
+//						mConversationArrayAdapter.clear()
+					}
+					BluetoothChatService.STATE_CONNECTING -> {
+					}//setStatus(R.string.title_connecting)
+					BluetoothChatService.STATE_LISTEN, BluetoothChatService.STATE_NONE -> {
+					}
+//						setStatus(
+//						R.string.title_not_connected
+//					)
+				}
+				Constants.MESSAGE_WRITE -> {
+					val writeBuf = msg.obj as ByteArray
+					// construct a string from the buffer
+					val writeMessage = String(writeBuf)
+					//mConversationArrayAdapter.add("Me:  $writeMessage")
+				}
+				Constants.MESSAGE_READ -> {
+					val readBuf = msg.obj as ByteArray
+					// construct a string from the valid bytes in the buffer
+					val readMessage = String(readBuf, 0, msg.arg1)
+					//mConversationArrayAdapter.add(mConnectedDeviceName + ":  " + readMessage)
+				}
+				Constants.MESSAGE_DEVICE_NAME -> {
+					// save the connected device's name
+					//mConnectedDeviceName = msg.data.getString(Constants.DEVICE_NAME)
+					if (null != activity) {
+						Toast.makeText(
+							activity,
+							"nome do device aqui",//"Connected to $mConnectedDeviceName",
+							Toast.LENGTH_SHORT
+						).show()
+					}
+				}
+				Constants.MESSAGE_TOAST -> if (null != activity) {
+					Toast.makeText(
+						activity, msg.data.getString(Constants.TOAST),
+						Toast.LENGTH_SHORT
+					).show()
+				}
+			}
+		}
 	}
 
 	companion object {
-		val TAG: String = ::BluetoothServerFragment.name
-		@JvmStatic
-		fun newInstance() =
-			BluetoothServerFragment()
+		private val TAG = ::BluetoothServerFragment.name
 	}
 }
