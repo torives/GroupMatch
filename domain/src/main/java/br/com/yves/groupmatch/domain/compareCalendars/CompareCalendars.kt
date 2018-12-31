059a8b671294170dc99f063ea8b952d3dee1156d
Victor Yves Crispim <vsilva@corp.globo.com>
2018-12-31 14:47:07 -0200

    [WIP] Remodelando MatchModels

--- domain/src/main/java/br/com/yves/groupmatch/domain/compareCalendars/CompareCalendars.kt
+++ domain/src/main/java/br/com/yves/groupmatch/domain/compareCalendars/CompareCalendars.kt
@@ -4,6 +4,38 @@ import br.com.yves.groupmatch.domain.UseCase
 import br.com.yves.groupmatch.domain.Week
 import br.com.yves.groupmatch.domain.createCalendar.CreateCalendar
 import br.com.yves.groupmatch.domain.sendCalendar.BusyCalendar
+import org.threeten.bp.LocalDateTime
+
+
+data class ClientCalendar(
+		val owner: String,
+		val dates: List<LocalDateTime>, //BusyDates
+		val weekStart: LocalDateTime,
+		val weekEnd: LocalDateTime
+)
+
+
+class MergedTimeSlotBuilder {
+	private lateinit var date: LocalDateTime
+	private lateinit var status: Map<MatchSessionMember, Boolean>
+
+	fun date(date: LocalDateTime) = apply { this.date = date }
+	fun status(status: Map<MatchSessionMember, Boolean>) = apply { this.status = status }
+
+	fun build(): MergedTimeSlot = MergedTimeSlot(date, status)
+}
+
+data class MergedTimeSlot(val date: LocalDateTime, val sessionMemberStatus: Map<MatchSessionMember, Boolean>) {
+	val isCompletelyBusy: Boolean
+		get() {
+			for (isBusy in sessionMemberStatus.values) {
+				if (isBusy.not()) {
+					return false
+				}
+			}
+			return true
+		}
+}
 
 class CompareCalendars(
 		private val calendars: List<BusyCalendar>,
@@ -19,7 +51,75 @@ class CompareCalendars(
 			throw IllegalArgumentException("Os calendários recebidos não estão na mesma semana")
 		}
 
-		val busyDates = calendars.flatMap { it.busySlots }.map { it.date }
+		//Cria mapa de dias ocupados para usuarios ocupados naquele dia
+		//Cria lista MergedTimeSlot
+		// Cria Span
+
+		val clientCalendars = listOf<ClientCalendar>()
+		val busyDates = mutableMapOf<LocalDateTime, MutableSet<MatchSessionMember>>()
+		clientCalendars.forEach { calendar ->
+			calendar.dates.forEach { date ->
+				val ownerList = busyDates[date]
+				if (ownerList == null) {
+					setOf(calendar.owner)
+				} else {
+					ownerList.add(MatchSessionMember(calendar.owner))
+				}
+			}
+		}
+
+//		val week = getReferenceWeekFrom(clientCalendars)
+//		val weekDates = dateRepository.getAllDatesFrom(week)
+		val allUsers = setOf<MatchSessionMember>()
+		val weekDates = listOf<LocalDateTime>()
+		val mergedTimeSlots = mutableListOf<MergedTimeSlot>()
+		val builder = MergedTimeSlotBuilder()
+		weekDates.forEach { date ->
+			builder.date(date)
+			val userSet = mutableMapOf<MatchSessionMember, Boolean>()
+
+			if (busyDates.containsKey(date)) {
+				busyDates[date]?.forEach { user ->
+					userSet[user] = true
+				}
+			} else {
+				allUsers.forEach { user ->
+					userSet[user] = false
+				}
+			}
+			builder.status(userSet)
+			mergedTimeSlots.add(builder.build())
+		}
+
+
+		//CREATE SPAN
+		val matchSlots = mutableListOf<MatchFreeSlot>()
+		val matchSlotBuilder = MatchSlotBuilder()
+		var isBuildingSpan = false
+		val memberStatus = mutableMapOf<MatchSessionMember, Boolean>()
+		mergedTimeSlots.forEachIndexed { index, mergedSlot ->
+			if (mergedSlot.isCompletelyBusy) {
+				if (isBuildingSpan) {
+					val endDate = mergedTimeSlots[index-1].date
+					matchSlotBuilder.end(endDate)
+					matchSlotBuilder.memberStatus(memberStatus)
+					matchSlots.add(matchSlotBuilder.build())
+
+					isBuildingSpan = false
+					memberStatus.clear()
+				}
+			} else {
+				if (isBuildingSpan) {
+
+				} else {
+					isBuildingSpan = true
+					matchSlotBuilder.start(mergedSlot.date)
+					memberStatus.putAll(mergedSlot.sessionMemberStatus)
+				}
+			}
+		}
+
+
 		val matchCalendar = createCalendar.with(week).execute()
 		matchCalendar.forEach { timeSlot ->
 			if (busyDates.contains(timeSlot.date)) {
--- domain/src/main/java/br/com/yves/groupmatch/domain/compareCalendars/MatchResult.kt
+++ domain/src/main/java/br/com/yves/groupmatch/domain/compareCalendars/MatchResult.kt
@@ -2,8 +2,34 @@ package br.com.yves.groupmatch.domain.compareCalendars
 
 import br.com.yves.groupmatch.domain.showCalendar.Calendar
 import br.com.yves.groupmatch.domain.showCalendar.TimeSlot
+import org.threeten.bp.LocalDateTime
 
 data class MatchResult(
 		val calendar: Calendar,
-		val result: Map<TimeSlot, List<User>?>? = null //TODO: registrar quem está ocupado em cada slot
+		val result: Map<TimeSlot, List<User>?>? = null //TODO: registrar quem está ocupado em cada slot,
 )
+
+
+typealias Match = List<MatchFreeSlot>
+
+data class MatchFreeSlot(
+		val start: LocalDateTime,
+		val end: LocalDateTime,
+		val sessionMembers: Set<MatchSessionMember>
+)
+
+
+class MatchSlotBuilder {
+	private lateinit var start: LocalDateTime
+	private lateinit var end: LocalDateTime
+	private lateinit var members: Set<MatchSessionMember>
+
+	fun start(date: LocalDateTime) = apply { this.start = date }
+	//TODO: Conferir se end >= start
+	fun end(date: LocalDateTime) = apply { this.end = date }
+	fun members(members: Set<MatchSessionMember>) = apply { this.members = members }
+
+	fun build(): MatchFreeSlot = MatchFreeSlot(start, end, members)
+}
+
+data class MatchSessionMember(val name: String)
