442b6166b596d51ad2f9bad9f9a170a29e3fb1c8
Victor Yves Crispim <vsilva@corp.globo.com>
2019-01-02 10:09:29 -0200

    Carregando o calendário local no ServerPresenter

--- presentation/src/main/java/br/com/yves/groupmatch/presentation/ui/bluetooth/server/ServerPresenter.kt
+++ presentation/src/main/java/br/com/yves/groupmatch/presentation/ui/bluetooth/server/ServerPresenter.kt
@@ -8,7 +8,7 @@ import android.util.Log
 import br.com.yves.groupmatch.BuildConfig
 import br.com.yves.groupmatch.R
 import br.com.yves.groupmatch.domain.compareCalendars.CompareCalendarsFactory
-import br.com.yves.groupmatch.domain.createCalendar.CreateCalendarFactory
+import br.com.yves.groupmatch.domain.loadCalendar.LoadCalendar
 import br.com.yves.groupmatch.domain.sendCalendar.ClientCalendar
 import br.com.yves.groupmatch.presentation.factory.DateRepositoryFactory
 import br.com.yves.groupmatch.presentation.ui.bluetooth.BluetoothMessageHandler
@@ -20,7 +20,8 @@ import kotlinx.coroutines.*
 
 class ServerPresenter(
 		private val view: ServerView,
-		private val bluetoothAdapter: BluetoothAdapter
+		private val bluetoothAdapter: BluetoothAdapter,
+		private val loadCalendar: LoadCalendar
 ) : BluetoothMessageHandler.Listener, CoroutineScope {
 	override val coroutineContext = Dispatchers.Default
 	private val bluetoothService = ServerBluetoothService(ServerBluetoothMessageHandler(this))
@@ -51,6 +52,7 @@ class ServerPresenter(
 		bluetoothService.pause()
 
 		// calcula o resultado
+		val localCalendar = loadCalendar.execute()
 		val compare = CompareCalendarsFactory.create(
 				receivedCalendars,
 				DateRepositoryFactory.create()
@@ -96,7 +98,7 @@ class ServerPresenter(
 	//region BluetoothMessageHandler.Listener
 	//TODO: identificar de que client é o calendar em questão para atualizar a lista
 	override fun onMessageRead(message: String) {
-		if(BuildConfig.DEBUG) {
+		if (BuildConfig.DEBUG) {
 			view.displayToast(message)
 			Log.d(TAG, message)
 		}
--- presentation/src/main/java/br/com/yves/groupmatch/presentation/ui/bluetooth/server/ServerPresenterFactory.kt
+++ presentation/src/main/java/br/com/yves/groupmatch/presentation/ui/bluetooth/server/ServerPresenterFactory.kt
@@ -1,7 +1,13 @@
 package br.com.yves.groupmatch.presentation.ui.bluetooth.server
 
 import android.bluetooth.BluetoothAdapter
+import br.com.yves.groupmatch.presentation.factory.TimeSlotRepositoryFactory
+import br.com.yves.groupmatch.presentation.factory.showCalendar.LoadCalendarFactory
 
 object ServerPresenterFactory {
-	fun create(view: ServerView) = ServerPresenter(view, BluetoothAdapter.getDefaultAdapter())
+	fun create(view: ServerView) = ServerPresenter(
+			view,
+			BluetoothAdapter.getDefaultAdapter(),
+			LoadCalendarFactory.create(TimeSlotRepositoryFactory.create())
+	)
 }
