b2bf90f5437cd37fcd759fcaff82651e8a200fce
Victor Yves Crispim <vsilva@corp.globo.com>
2019-01-02 10:08:57 -0200

    Removendo código não utilizado de TimeSlot

--- domain/src/main/java/br/com/yves/groupmatch/domain/loadCalendar/TimeSlot.kt
+++ domain/src/main/java/br/com/yves/groupmatch/domain/loadCalendar/TimeSlot.kt
@@ -5,10 +5,4 @@ import org.threeten.bp.LocalDateTime
 data class TimeSlot(
 	val date: LocalDateTime,
 	var isBusy: Boolean
-) {
-	fun merge(timeSlot: TimeSlot) {
-		if(timeSlot.isBusy) {
-			this.isBusy = true
-		}
-	}
-}
+)
