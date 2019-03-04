2ee09b4ac4d9095c2c52b6e75cb87eeb76b3a617
Victor Yves Crispim <rotcivyves@gmail.com>
2019-03-04 18:40:15 -0300

    [WIP] Comparando calendarios
    - Ontem o diff de busy dos calendarios

--- domain/src/main/java/br/com/yves/groupmatch/domain/compareCalendars/CompareCalendars.kt
+++ domain/src/main/java/br/com/yves/groupmatch/domain/compareCalendars/CompareCalendars.kt
@@ -1,16 +1,24 @@
 package br.com.yves.groupmatch.domain.compareCalendars
 
 import br.com.yves.groupmatch.domain.DateRepository
-import br.com.yves.groupmatch.domain.models.timeslot.TimeSlot
 import br.com.yves.groupmatch.domain.UseCase
-import br.com.yves.groupmatch.domain.models.Week
 import br.com.yves.groupmatch.domain.models.calendar.Calendar
+import br.com.yves.groupmatch.domain.models.timeslot.Slot
+import br.com.yves.groupmatch.domain.models.timeslot.TimeSlot
 import org.threeten.bp.LocalDateTime
 
 class CompareCalendars(
 		private val calendars: List<Calendar>,
 		private val dateRepository: DateRepository
 ) : UseCase<CalendarMatch>() {
+	init {
+		require(calendars.isNotEmpty()) {
+			"Failed to instantiate ${this::class.java.name}. Can't compare calendars with empty calendar list"
+		}
+		require(areCalendarsComparable()) {
+			"Failed to instantiate ${this::class.java.name}. Check if calendars reference the same week and have the same number of ${TimeSlot::class.java.simpleName}s"
+		}
+	}
 
 	private class MergedTimeSlotBuilder {
 		private lateinit var date: LocalDateTime
@@ -24,44 +32,28 @@ class CompareCalendars(
 
 	private data class MergedTimeSlot(val date: LocalDateTime, val sessionMemberStatus: Map<SessionMember, Boolean>)
 
-//	data class MatchFreeSlot(
-//			override val start: LocalDateTime,
-//			override val end: LocalDateTime,
-//			val sessionMembers: Set<SessionMember> = mutableSetOf()
-//	) : TimeSlot() {
-//		override val isBusy: Boolean = false
-//
-//		init {
-//			require(start < end) {
-//				"Failed to instantiate ${this::class.java.name}. Start date ($start) must be smaller than end date ($end)"
-//			}
-//		}
-//
-//		fun canMerge(other: MatchFreeSlot): Boolean {
-//			return other.end == this.start || other.start == this.end
-//		}
-//
-//		fun merge(other: MatchFreeSlot): MatchFreeSlot {
-//			require(other.end == this.start || other.start == this.end) {
-//				"Cannot merge slots that aren't contiguous"
-//			}
-//			val ordered = listOf(this, other).sortedBy { it.start }
-//			val newMembers = this.sessionMembers.intersect(other.sessionMembers)
-//
-//			return MatchFreeSlot(ordered.first().start, ordered.last().end, newMembers)
-//		}
-//	}
 
 	private val allSessionUsers: Set<SessionMember> by lazy {
 		calendars.map { SessionMember(it.owner) }.toSet()
 	}
 
 	override fun execute(): CalendarMatch {
-//		check(calendars.isNotEmpty()) {
-//			"Cannot compare calendars with empty calendar list"
-//		}
-//
-//		val weekDates = getAllDatesFromReferenceWeek(calendars)
+
+		val week = calendars.first().week
+		val resultSlots = calendars.first().timeSlots.map { MatchTimeSlot(it.start, it.end, it.isBusy) }
+		val timeSlots = calendars.map { it.timeSlots }
+
+		resultSlots.forEachIndexed { index, slot ->
+			timeSlots.forEach { timeSlots ->
+				slot.isBusy = slot.isBusy || timeSlots[index].isBusy
+			}
+		}
+
+		resultSlots.forEach { slot ->
+			
+		}
+
+
 //		val busyDates = mapBusyDatesToMatchSessionMember(calendars)
 //		val mergedTimeSlots = createMergedTimeSlots(weekDates, busyDates)
 //
@@ -75,19 +67,19 @@ class CompareCalendars(
 //
 //		val trueSlots = freeMergedSlots.map {
 //			/*
-//			 cada MatchFreeSlot representa um range de horário. TimeSlot representa um limite de horãrio apenas.
-//			 ent"ao pra contruir um MatchFreeSlot vc precisa de dois TimeSlots.
+//			 cada MatchTimeSlot representa um range de horário. TimeSlot representa um limite de horãrio apenas.
+//			 ent"ao pra contruir um MatchTimeSlot vc precisa de dois TimeSlots.
 //
 //			 se o primeiro timeslot é livre e o segundo eh ocupado
-//			 é um MatchFreeSlot
+//			 é um MatchTimeSlot
 //
 //			 se o primeiro timeslot é ocupado e o segundo é livre,
-//			 não ~~ um MatchFreeSlot
+//			 não ~~ um MatchTimeSlot
 //			* */
-//			MatchFreeSlot(freeMergedSlots.first().first, freeMergedSlots.first().first.plusHours(1), freeMergedSlots.first().second)
+//			MatchTimeSlot(freeMergedSlots.first().first, freeMergedSlots.first().first.plusHours(1), freeMergedSlots.first().second)
 //		}
 //		trueSlots.sortedBy { it.start }
-//		val result = mutableListOf<MatchFreeSlot>()
+//		val result = mutableListOf<MatchTimeSlot>()
 //		var current = trueSlots.first()
 //		result.add(current)
 //		for (i in 1 until trueSlots.lastIndex) {
@@ -96,16 +88,14 @@ class CompareCalendars(
 //			current = if (current.canMerge(next)) {
 //				val new = current.merge(next)
 //				result.add(new)
-//
 //				new
 //			} else {
 //				next
 //			}
 //		}
-//
-//		result.sortedBy { it.sessionMembers.size }
+//		val sortedResult = result.sortedBy { it.sessionMembers.size }
 
-		return CalendarMatch(mapOf())
+		return CalendarMatch()
 	}
 
 	private fun createMergedTimeSlots(
@@ -121,7 +111,7 @@ class CompareCalendars(
 			val userSet = mutableMapOf<SessionMember, Boolean>()
 
 			if (busyDates.containsKey(date)) {
-				busyDates[date]!!.forEach { user ->
+				busyDates.getValue(date).forEach { user ->
 					userSet[user] = true
 				}
 			} else {
@@ -135,9 +125,29 @@ class CompareCalendars(
 		return mergedTimeSlots
 	}
 
-	private fun getAllDatesFromReferenceWeek(calendars: List<Calendar>): List<LocalDateTime> {
-		val referenceWeek = getReferenceWeek(calendars)
-		return dateRepository.getAllDatesFrom(referenceWeek)
+
+	private fun areCalendarsComparable(): Boolean {
+		return areCalendarsWellFormed() && areCalendarsReferencingSameWeek()
+	}
+
+	private fun areCalendarsWellFormed(): Boolean {
+		for (calendar in calendars) {
+			if (calendar.timeSlots.size != TIMESLOTS_PER_WEEK)
+				return false
+		}
+		return true
+	}
+
+	private fun areCalendarsReferencingSameWeek(): Boolean {
+		val weeks = calendars.map { it.week }
+		val current = weeks.first()
+
+		for (i in 1 until weeks.lastIndex) {
+			if (current != weeks[i]) {
+				return false
+			}
+		}
+		return true
 	}
 
 	private fun mapBusyDatesToMatchSessionMember(calendars: List<Calendar>): Map<LocalDateTime, Set<SessionMember>> {
@@ -145,7 +155,7 @@ class CompareCalendars(
 
 		calendars.forEach { calendar ->
 			calendar.timeSlots.forEach { slot ->
-				if (slot.isBusy){
+				if (slot.isBusy) {
 					busyDates[slot.start]?.apply {
 						add(SessionMember(calendar.owner))
 					} ?: run {
@@ -157,15 +167,51 @@ class CompareCalendars(
 		return busyDates
 	}
 
-	private fun getReferenceWeek(calendars: List<Calendar>): Week {
-		val weeks = calendars.map { it.week }
-		val current = weeks.first()
-
-		for (i in 1 until weeks.lastIndex) {
-			if (current != weeks[i]) {
-				throw IllegalArgumentException("Cannot compare calendars with different reference weeks")
-			}
-		}
-		return current
+	companion object {
+		private const val TIMESLOTS_PER_WEEK = 168 //24h per day, 7 days per week
 	}
 }
+
+data class MatchTimeSlot(
+		override val start: LocalDateTime,
+		override val end: LocalDateTime,
+		override var isBusy: Boolean
+) : Slot {
+	init {
+		require(start < end) {
+			"Failed to instantiate ${this::class.java.name}. Start date ($start) must be smaller than end date ($end)"
+		}
+	}
+
+	/**
+	 * Merges **this** with another contiguous [MatchTimeSlot], possibly altering its [isBusy] status.
+	 *
+	 * @param other The [MatchTimeSlot] that will be merged with **this**
+	 *
+	 * @return A new [MatchTimeSlot], starting from the earliest date and ending on the furthest
+	 * date between **this** and [other].
+	 * The new slot is busy if **this** or [other] is busy
+	 *
+	 * @throws IllegalArgumentException If the slots aren't contiguous or have a different busy status
+	 */
+	fun merge(other: Slot): MatchTimeSlot {
+		require(canMerge(other)) {
+			"Cannot merge slots that aren't contiguous"
+		}
+		val ordered = listOf(this, other).sortedBy { it.start }
+
+		return MatchTimeSlot(
+				ordered.first().start,
+				ordered.last().end,
+				isBusy = ordered.first().isBusy
+		)
+	}
+
+	private fun canMerge(other: Slot): Boolean {
+		return (other.end == this.start || other.start == this.end) &&
+				this.isBusy == other.isBusy
+	}
+}
+
+
+class CalendarMatch
--- domain/src/main/java/br/com/yves/groupmatch/domain/models/timeslot/TimeSlot.kt
+++ domain/src/main/java/br/com/yves/groupmatch/domain/models/timeslot/TimeSlot.kt
@@ -4,13 +4,22 @@ import org.threeten.bp.LocalDateTime
 
 data class TimeSlot(
 		val calendarId: Long = 0,
-		val start: LocalDateTime,
-		val end: LocalDateTime,
-		var isBusy: Boolean
-) {
+		override val start: LocalDateTime,
+		override val end: LocalDateTime,
+		override var isBusy: Boolean
+): Slot {
 	init {
 		require(start < end) {
 			"Failed to instantiate ${this::class.java.name}. Start date ($start) must be smaller than end date ($end)"
 		}
+		require(start.plusHours(1) == end) {
+			"Failed to instantiate ${this::class.java.name}. ${this::class.java.name}s must have 1h between start and end"
+		}
 	}
+}
+
+interface Slot {
+	val start: LocalDateTime
+	val end: LocalDateTime
+	var isBusy: Boolean
 }
