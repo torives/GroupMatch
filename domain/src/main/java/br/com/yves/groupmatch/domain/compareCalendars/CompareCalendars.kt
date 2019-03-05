04c1a01e5890f28e1d170fcbb597fa0e90ede979
Victor Yves Crispim <rotcivyves@gmail.com>
2019-03-04 21:23:39 -0300

    [WIP] Refatorando métodos

--- domain/src/main/java/br/com/yves/groupmatch/domain/compareCalendars/CompareCalendars.kt
+++ domain/src/main/java/br/com/yves/groupmatch/domain/compareCalendars/CompareCalendars.kt
@@ -20,132 +20,53 @@ class CompareCalendars(
 		}
 	}
 
-	private class MatchTimeSlotBuilder {
-		var start: LocalDateTime? = null
-			private set
-		var end: LocalDateTime? = null
-			private set
-
-		fun setStart(start: LocalDateTime) = apply { this.start = start }
-		fun setEnd(end: LocalDateTime) = apply { this.end = end }
-
-		fun build(): MatchTimeSlot {
-			val slot = MatchTimeSlot(start!!, end!!, false)
-			start = null
-			end = null
-
-			return slot
-		}
-	}
-
-	private data class MergedTimeSlot(val date: LocalDateTime, val sessionMemberStatus: Map<SessionMember, Boolean>)
-
-
-	private val allSessionUsers: Set<SessionMember> by lazy {
-		calendars.map { SessionMember(it.owner) }.toSet()
-	}
-
 	override fun execute(): CalendarMatch {
 
-//		val week = calendars.first().week
-//		val weekDates = dateRepository.getAllDatesFrom(week)
-//		val weekDays = weekDates.groupBy { it.dayOfWeek }
-
-
-
-
-
-
-		val resultSlots = calendars.first().timeSlots.map { MatchTimeSlot(it.start, it.end, it.isBusy) }
-		val timeSlots = calendars.map { it.timeSlots }
-
-		resultSlots.forEachIndexed { index, slot ->
-			timeSlots.forEach { timeSlots ->
-				slot.isBusy = slot.isBusy || timeSlots[index].isBusy
-			}
-		}
-
-
-		val result = mutableListOf<MatchTimeSlot>()
-		var builder = MatchTimeSlotBuilder()
-		resultSlots.forEachIndexed { index, slot ->
-			if (builder.start != null){
-				when {
-					slot.isBusy -> {
-						builder.setEnd(slot.start)
-						result.add(builder.build())
-					}
-					slot.start.dayOfWeek != builder.start?.dayOfWeek -> {
-						builder.setEnd(slot.start)
-						result.add(builder.build())
-						builder.setStart(slot.start)
-					}
-					resultSlots.indices.last == index -> {
-						builder.setEnd(slot.end)
-						result.add(builder.build())
-					}
+		fun consolidateBusyStatus(matchSlots: List<MatchTimeSlot>, timeSlots: List<List<TimeSlot>>) {
+			matchSlots.forEachIndexed { index, slot ->
+				timeSlots.forEach { timeSlots ->
+					slot.isBusy = slot.isBusy || timeSlots[index].isBusy
 				}
-			} else if (!slot.isBusy){
-				builder.setStart(slot.start)
 			}
 		}
 
-//			if (slot.isBusy.not()) {
-//				if (builder.start == null) {
-//					builder.setStart(slot.start)
-//				} else if (slot.start.dayOfWeek == builder.start?.dayOfWeek) {
-//
-//				}
-//			} else {
-//				if (builder.start != null || slot.start.dayOfWeek != builder.start?.dayOfWeek) {
-//					builder.setEnd(slot.start)
-//					result.add(builder.build())
-//				}
-//			}
-//		}
-		print(result)
+		fun generateMatchResult(consolidatedSlots: List<MatchTimeSlot>): List<MatchTimeSlot> {
+			val result = mutableListOf<MatchTimeSlot>()
+			val builder = MatchTimeSlotBuilder()
 
+			fun closeAndBuild(end: LocalDateTime) {
+				builder.setEnd(end)
+				result.add(builder.build())
+				builder.clear()
+			}
 
-//		val busyDates = mapBusyDatesToMatchSessionMember(calendars)
-//		val mergedTimeSlots = createMergedTimeSlots(weekDates, busyDates)
-//
-//		//FreeSlots
-//		val freeMergedSlots = mergedTimeSlots.map { timeSlot ->
-//			val freeMembers = timeSlot.sessionMemberStatus.mapNotNull { (member, isBusy) ->
-//				if (isBusy.not()) member else null
-//			}
-//			Pair(timeSlot.date, freeMembers.toSet())
-//		}
-//
-//		val trueSlots = freeMergedSlots.map {
-//			/*
-//			 cada MatchTimeSlot representa um range de horário. TimeSlot representa um limite de horãrio apenas.
-//			 ent"ao pra contruir um MatchTimeSlot vc precisa de dois TimeSlots.
-//
-//			 se o primeiro timeslot é livre e o segundo eh ocupado
-//			 é um MatchTimeSlot
-//
-//			 se o primeiro timeslot é ocupado e o segundo é livre,
-//			 não ~~ um MatchTimeSlot
-//			* */
-//			MatchTimeSlot(freeMergedSlots.first().first, freeMergedSlots.first().first.plusHours(1), freeMergedSlots.first().second)
-//		}
-//		trueSlots.sortedBy { it.start }
-//		val result = mutableListOf<MatchTimeSlot>()
-//		var current = trueSlots.first()
-//		result.add(current)
-//		for (i in 1 until trueSlots.lastIndex) {
-//			val next = trueSlots[i]
-//
-//			current = if (current.canMerge(next)) {
-//				val new = current.merge(next)
-//				result.add(new)
-//				new
-//			} else {
-//				next
-//			}
-//		}
-//		val sortedResult = result.sortedBy { it.sessionMembers.size }
+			consolidatedSlots.forEachIndexed { index, slot ->
+				if (!builder.isEmpty) {
+					when {
+						slot.isBusy -> {
+							closeAndBuild(slot.start)
+						}
+						slot.start.dayOfWeek != builder.start?.dayOfWeek -> {
+							closeAndBuild(slot.start)
+							builder.setStart(slot.start)
+						}
+						consolidatedSlots.indices.last == index -> {
+							closeAndBuild(slot.end)
+						}
+					}
+				} else if (!slot.isBusy) {
+					builder.setStart(slot.start)
+				}
+			}
+			return result
+		}
+
+		val matchSlots = calendars.first().timeSlots.map { MatchTimeSlotMapper.map(it) }
+		val calendarTimeSlots = calendars.map { it.timeSlots }
+
+		consolidateBusyStatus(matchSlots, calendarTimeSlots)
+		val matchResult = generateMatchResult(matchSlots)
+
 
 		return CalendarMatch()
 	}
@@ -172,23 +93,6 @@ class CompareCalendars(
 			}
 		}
 		return true
-	}
-
-	private fun mapBusyDatesToMatchSessionMember(calendars: List<Calendar>): Map<LocalDateTime, Set<SessionMember>> {
-		val busyDates = mutableMapOf<LocalDateTime, MutableSet<SessionMember>>()
-
-		calendars.forEach { calendar ->
-			calendar.timeSlots.forEach { slot ->
-				if (slot.isBusy) {
-					busyDates[slot.start]?.apply {
-						add(SessionMember(calendar.owner))
-					} ?: run {
-						busyDates[slot.start] = mutableSetOf(SessionMember(calendar.owner))
-					}
-				}
-			}
-		}
-		return busyDates
 	}
 
 	companion object {
@@ -235,6 +139,40 @@ data class MatchTimeSlot(
 		return (other.end == this.start || other.start == this.end) &&
 				this.isBusy == other.isBusy
 	}
+}
+
+private class MatchTimeSlotBuilder {
+	var start: LocalDateTime? = null
+		private set
+	var end: LocalDateTime? = null
+		private set
+	var isEmpty = true
+
+	fun setStart(start: LocalDateTime) = apply {
+		this.start = start
+		isEmpty = false
+	}
+
+	fun setEnd(end: LocalDateTime) = apply {
+		this.end = end
+		isEmpty = false
+	}
+
+	fun build() = MatchTimeSlot(start!!, end!!, false)
+
+	fun clear() {
+		start = null
+		end = null
+		isEmpty = true
+	}
+}
+
+object MatchTimeSlotMapper {
+	fun map(slot: Slot): MatchTimeSlot = MatchTimeSlot(
+			slot.start,
+			slot.end,
+			slot.isBusy
+	)
 }
 
 
