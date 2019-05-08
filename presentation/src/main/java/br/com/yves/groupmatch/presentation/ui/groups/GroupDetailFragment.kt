8a2016720b0d9c329cdcb7884e3ac3f325c8ea07
Victor Yves Crispim <rotcivyves@gmail.com>
2019-05-08 00:01:13 -0300

    Configurando a Toolbar

--- presentation/src/main/java/br/com/yves/groupmatch/presentation/ui/groups/GroupDetailFragment.kt
+++ presentation/src/main/java/br/com/yves/groupmatch/presentation/ui/groups/GroupDetailFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import br.com.yves.groupmatch.R
+import kotlinx.android.synthetic.main.fragment_group_detail.*
 
 /**
  * A simple [Fragment] subclass.
@@ -17,6 +19,12 @@ class GroupDetailFragment : Fragment() {
 	                          savedInstanceState: Bundle?): View? {
 		// Inflate the layout for this fragment
 		return inflater.inflate(R.layout.fragment_group_detail, container, false)
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+
+//		(activity as AppCompatActivity).setSupportActionBar(toolbar)
 	}
 
 
--- presentation/src/main/java/br/com/yves/groupmatch/presentation/ui/main/MainActivity.kt
+++ presentation/src/main/java/br/com/yves/groupmatch/presentation/ui/main/MainActivity.kt
@@ -3,6 +3,7 @@ package br.com.yves.groupmatch.presentation.ui.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import br.com.yves.groupmatch.R
 import br.com.yves.groupmatch.data.auth.GroupMatchAuth
@@ -22,10 +23,15 @@ class MainActivity : AppCompatActivity() {
 	private fun setupNavigation() {
 		val navController = navHost.findNavController()
 
+		val appBarConfiguration = AppBarConfiguration(
+				setOf(
+						R.id.groupsFragment,
+						R.id.calendarFragment,
+						R.id.accountFragment
+				)
+		)
+		toolbar.setupWithNavController(navController, appBarConfiguration)
 		bottomNavigationView.setupWithNavController(navController)
-
-		//FIXME: Descobrir como esconder o Up Button nos primeiros fragments das abas
-//        setupActionBarWithNavController(navController)
 	}
 
 	override fun onSupportNavigateUp() =
