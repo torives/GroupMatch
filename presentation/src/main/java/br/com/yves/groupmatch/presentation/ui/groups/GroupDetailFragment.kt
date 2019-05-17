c09197ffaf3d5bc9d511717c00eb50aa41abcbf2
Victor Yves Crispim <rotcivyves@gmail.com>
2019-05-16 22:03:54 -0300

    Adiciona métodos na GroupDetailView

--- presentation/src/main/java/br/com/yves/groupmatch/presentation/ui/groups/GroupDetailFragment.kt
+++ presentation/src/main/java/br/com/yves/groupmatch/presentation/ui/groups/GroupDetailFragment.kt
@@ -8,15 +8,19 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import br.com.yves.groupmatch.R
+import br.com.yves.groupmatch.presentation.ui.account.UserViewModel
 
 interface GroupDetailView {
-
+	fun displayMatchButton()
+	fun hideMatchButton()
+	fun displayGroupMembers(members: List<UserViewModel>)
 }
 
 class GroupDetailFragment : Fragment(), GroupDetailView {
 
 	private val args: GroupDetailFragmentArgs by navArgs()
 
+	//region Lifecycle
 	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
 	                          savedInstanceState: Bundle?): View? {
 		return inflater.inflate(R.layout.fragment_group_detail, container, false)
@@ -26,4 +30,20 @@ class GroupDetailFragment : Fragment(), GroupDetailView {
 		super.onViewCreated(view, savedInstanceState)
 
 	}
+	//endregion
+
+	//region GroupDetailView
+	override fun displayMatchButton() {
+		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+	}
+
+	override fun hideMatchButton() {
+		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+	}
+
+	override fun displayGroupMembers(members: List<UserViewModel>) {
+		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+	}
+	//endregion
+
 }
