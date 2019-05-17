64385601a1c6ca279a3f86c666400ba9037b754a
Victor Yves Crispim <rotcivyves@gmail.com>
2019-05-16 22:50:10 -0300

    Cria GroupMemberViewHolder

--- presentation/src/main/java/br/com/yves/groupmatch/presentation/ui/groups/details/GroupMemberAdapter.kt
+++ presentation/src/main/java/br/com/yves/groupmatch/presentation/ui/groups/details/GroupMemberAdapter.kt
@@ -1,17 +1,19 @@
 package br.com.yves.groupmatch.presentation.ui.groups.details
 
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import br.com.yves.groupmatch.presentation.ui.account.UserViewModel
 import com.bumptech.glide.RequestManager
+import kotlinx.android.extensions.LayoutContainer
 
 class GroupMemberAdapter(
 		private var members: List<UserViewModel>? = null,
 		private val glide: RequestManager
-) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+) : RecyclerView.Adapter<GroupMemberAdapter.GroupMemberViewHolder>() {
 
 	//region RecyclerView.Adapter
-	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GroupMemberViewHolder {
 		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
 	}
 
@@ -19,9 +21,19 @@ class GroupMemberAdapter(
 		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
 	}
 
-	override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+	override fun onBindViewHolder(holder: GroupMemberViewHolder, position: Int) {
 		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
 	}
 	//endregion
+
+	inner class GroupMemberViewHolder(view: View): RecyclerView.ViewHolder(view), LayoutContainer {
+		override val containerView: View?
+			get() = itemView
+
+		fun bind(member: UserViewModel) {
+
+		}
+	}
+
 
 }
