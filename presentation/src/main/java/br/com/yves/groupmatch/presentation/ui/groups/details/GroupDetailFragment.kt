3606edf41cb502e620135f4b0e3ac08b69f6ce09
Victor Yves Crispim <rotcivyves@gmail.com>
2019-05-16 22:35:26 -0300

    Implementa display e hide MatchButton

--- presentation/src/main/java/br/com/yves/groupmatch/presentation/ui/groups/details/GroupDetailFragment.kt
+++ presentation/src/main/java/br/com/yves/groupmatch/presentation/ui/groups/details/GroupDetailFragment.kt
@@ -4,11 +4,15 @@ package br.com.yves.groupmatch.presentation.ui.groups.details
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import br.com.yves.groupmatch.R
+import br.com.yves.groupmatch.presentation.runOnUiThread
 import br.com.yves.groupmatch.presentation.ui.account.UserViewModel
+import kotlinx.android.synthetic.main.fragment_group_detail.*
 
 interface GroupDetailView {
 	fun displayMatchButton()
@@ -33,12 +37,12 @@ class GroupDetailFragment : Fragment(), GroupDetailView {
 	//endregion
 
 	//region GroupDetailView
-	override fun displayMatchButton() {
-		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+	override fun displayMatchButton() = runOnUiThread {
+		group_details_answerMatch.visibility = VISIBLE
 	}
 
-	override fun hideMatchButton() {
-		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+	override fun hideMatchButton() = runOnUiThread {
+		group_details_answerMatch.visibility = GONE
 	}
 
 	override fun displayGroupMembers(members: List<UserViewModel>) {
