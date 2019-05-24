82971960d688a55f4e005dde5ebf723c52852445
Victor Yves Crispim <rotcivyves@gmail.com>
2019-05-23 22:33:14 -0300

    Renomeia GroupFirestoreRepository para GroupFirebaseRepository

--- data/src/main/java/br/com/yves/groupmatch/data/group/GroupFirebaseRepository.kt
+++ data/src/main/java/br/com/yves/groupmatch/data/group/GroupFirebaseRepository.kt
@@ -1,9 +1,6 @@
 package br.com.yves.groupmatch.data.group
 
 import android.util.Log
-import br.com.yves.groupmatch.domain.group.Group
-import br.com.yves.groupmatch.domain.group.GroupRepository
-import br.com.yves.groupmatch.domain.models.account.User
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
@@ -11,28 +8,28 @@ import kotlinx.coroutines.withContext
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
-class GroupFirestoreRepository : GroupRepository {
+class GroupFirebaseRepository {
 
 	private val db: FirebaseFirestore
 		get() = FirebaseFirestore.getInstance()
 
 	//region GroupRepository
-	override fun getGroup(groupId: String): Group? = runBlocking {
+	fun getGroup(groupId: String): FirebaseGroup? = runBlocking {
 		return@runBlocking withContext(Dispatchers.IO) {
 			getGroupSynchronous(groupId)
 		}
 	}
 
-	override fun getAllGroups(userId: String): List<Group> {
+	fun getAllGroups(userId: String): List<FirebaseGroup> {
 		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
 	}
 	//endregion
 
-	private suspend fun getGroupSynchronous(groupId: String): Group? = suspendCoroutine { cont ->
+	private suspend fun getGroupSynchronous(groupId: String): FirebaseGroup? = suspendCoroutine { cont ->
 		db.collection(COLLECTION_GROUPS).document(groupId).get()
 				.addOnSuccessListener {
-					val group = it.toObject(FirestoreGroup::class.java)
-//					cont.resume(Group("", "","", emptyList(), User(), )
+					val group = it.toObject(FirebaseGroup::class.java)
+					cont.resume(group)
 				}
 				.addOnFailureListener {
 					Log.d(TAG, "Failed to retrieve group with id: $groupId")
@@ -41,7 +38,7 @@ class GroupFirestoreRepository : GroupRepository {
 	}
 
 	companion object {
-		private val TAG = GroupFirestoreRepository::class.java.simpleName
+		private val TAG = GroupFirebaseRepository::class.java.simpleName
 		private const val COLLECTION_GROUPS = "groups"
 		private const val COLLECTION_GROUPS_ID = "id"
 	}
