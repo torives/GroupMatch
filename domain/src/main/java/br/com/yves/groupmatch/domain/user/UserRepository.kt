f378fc21577615b92fdaf9e979dab1476834c231
Victor Yves Crispim <rotcivyves@gmail.com>
2019-06-02 16:41:02 -0300

    Cria erros

--- domain/src/main/java/br/com/yves/groupmatch/domain/user/UserRepository.kt
+++ domain/src/main/java/br/com/yves/groupmatch/domain/user/UserRepository.kt
@@ -1,5 +1,7 @@
 package br.com.yves.groupmatch.domain.user
 
+import java.lang.Exception
+
 interface UserRepository {
 	fun getUser(uid: String, callback: GetUserCallback)
 	fun updateUser(user: User, callback: UpdateUserCallback)
@@ -13,4 +15,27 @@ interface UserRepository {
 		fun onSuccess()
 		fun onFailure(error: Error)
 	}
+
+
+}
+
+open class GroupMatchError(
+		val code: Int,
+		message: String,
+		val exception: Exception? = null
+): Error(message, exception)
+
+class APIError(
+		code: Int,
+		message: String,
+		exception: Exception? = null
+): GroupMatchError(code, message, exception)
+
+sealed class UserRepositoryError(
+		code: Int,
+		message: String,
+		exception: Exception? = null
+): GroupMatchError(code, message, exception) {
+
+	class InexistentUser: UserRepositoryError(1, "User does not exists")
 }
