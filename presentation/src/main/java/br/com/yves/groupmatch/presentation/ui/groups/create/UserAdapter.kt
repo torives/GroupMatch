033bd45ad1b211fda0d6c28ad97be3f704047e96
Victor Yves Crispim <rotcivyves@gmail.com>
2019-06-05 22:30:48 -0300

    [WIP] Implementação da lista particionada

--- presentation/src/main/java/br/com/yves/groupmatch/presentation/ui/groups/create/UserAdapter.kt
+++ presentation/src/main/java/br/com/yves/groupmatch/presentation/ui/groups/create/UserAdapter.kt
@@ -5,24 +5,48 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.extensions.LayoutContainer
 
-class UserAdapter: RecyclerView.Adapter<UserAdapter.UserListViewHolder>() {
+class UserAdapter(
+		private var items: List<UserViewModel> = listOf(),
+		private val listener: Listener
+) : RecyclerView.Adapter<UserAdapter.UserListViewHolder>() {
+
+	private var selectedUsers = mutableListOf<UserListItem.User>()
+	private var users = mutableListOf<UserListItem.User>()
+
+	//region RecyclerView.Adapter
 	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserListViewHolder {
 		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
 	}
 
 	override fun getItemViewType(position: Int): Int {
-		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+		return if(position == 0) {
+			if(selectedUsers.isEmpty()) {
+
+			}
+			UserListItem.Header.type
+		} else {
+			return 0
+		}
 	}
 
 	override fun getItemCount(): Int {
-		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+		return if(selectedUsers.isEmpty()) {
+			users.size + 1
+		} else {
+			selectedUsers.size + users.size + 2
+		}
 	}
 
 	override fun onBindViewHolder(holder: UserListViewHolder, position: Int) {
 		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
 	}
+	//endregion
 
-	abstract inner class UserListViewHolder(view: View): RecyclerView.ViewHolder(view), LayoutContainer {
+	interface Listener {
+		fun onItemSelected(item: UserListItem)
+	}
+
+	abstract inner class UserListViewHolder(view: View) : RecyclerView.ViewHolder(view), LayoutContainer {
 		override val containerView: View?
 			get() = itemView
 
@@ -31,4 +55,26 @@ class UserAdapter: RecyclerView.Adapter<UserAdapter.UserListViewHolder>() {
 }
 
 
-sealed class UserListItem
+sealed class UserListItem(val type: Int) {
+	data class Header(val title: String) : UserListItem(type) {
+		companion object {
+			const val type = 1
+		}
+	}
+
+	data class User(
+			val viewModel: UserViewModel,
+			var isSelected: Boolean
+	) : UserListItem(type) {
+		companion object {
+			const val type = 2
+		}
+	}
+}
+
+class UserViewModel(
+		val name: String,
+		val email: String,
+		var isSelected: Boolean,
+		val profileImageURL: String? = null
+)
