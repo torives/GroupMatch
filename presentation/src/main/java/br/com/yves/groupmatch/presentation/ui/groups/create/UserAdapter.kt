4708420484b913364e01e0b459628b2b2d3d7ae9
Victor Yves Crispim <rotcivyves@gmail.com>
2019-06-05 21:56:02 -0300

    Cria UserListViewHolder

--- presentation/src/main/java/br/com/yves/groupmatch/presentation/ui/groups/create/UserAdapter.kt
+++ presentation/src/main/java/br/com/yves/groupmatch/presentation/ui/groups/create/UserAdapter.kt
@@ -1,10 +1,16 @@
 package br.com.yves.groupmatch.presentation.ui.groups.create
 
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.extensions.LayoutContainer
 
-class UserAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+class UserAdapter: RecyclerView.Adapter<UserAdapter.UserListViewHolder>() {
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserListViewHolder {
+		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+	}
+
+	override fun getItemViewType(position: Int): Int {
 		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
 	}
 
@@ -12,8 +18,17 @@ class UserAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
 	}
 
-	override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+	override fun onBindViewHolder(holder: UserListViewHolder, position: Int) {
 		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
 	}
 
+	abstract inner class UserListViewHolder(view: View): RecyclerView.ViewHolder(view), LayoutContainer {
+		override val containerView: View?
+			get() = itemView
+
+		abstract fun bind(item: UserListItem)
+	}
 }
+
+
+sealed class UserListItem
