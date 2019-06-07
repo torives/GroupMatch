f5ac870e6722ca0e36beced9842e86e3b64850c9
Victor Yves Crispim <rotcivyves@gmail.com>
2019-06-06 22:32:42 -0300

    Cria o layout do NewGroupDetailFragment

--- presentation/src/main/java/br/com/yves/groupmatch/debug/NewGroupDetails.kt
+++ presentation/src/main/java/br/com/yves/groupmatch/debug/NewGroupDetails.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import br.com.yves.groupmatch.R
 
 class NewGroupDetails : Fragment() {
@@ -15,5 +16,8 @@ class NewGroupDetails : Fragment() {
 		return inflater.inflate(R.layout.fragment_new_group_details, container, false)
 	}
 
-
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		(activity as AppCompatActivity).supportActionBar?.subtitle = "Adicionar nome"
+	}
 }
