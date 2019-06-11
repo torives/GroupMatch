fd8f93f3c5fc17d2e7c9eafad282daa672187a53
Victor Yves Crispim <rotcivyves@gmail.com>
2019-06-11 07:08:45 -0300

    Altera UserRepository

--- presentation/src/main/java/br/com/yves/groupmatch/presentation/ui/groups/create/NewGroupController.kt
+++ presentation/src/main/java/br/com/yves/groupmatch/presentation/ui/groups/create/NewGroupController.kt
@@ -8,7 +8,7 @@ class NewGroupController(
 		view: NewGroupView,
 		private val repository: UserRepository,
 		private val presenter: UserPresenter
-) : UserRepository.GetAllUsersCallback {
+) : UserRepository.GetUsersCallback {
 
 	private val selectedUsers = mutableSetOf<UserViewModel>()
 	private lateinit var users: List<UserViewModel>
@@ -49,7 +49,7 @@ class NewGroupController(
 		}
 	}
 
-	//region GetAllUsersCallback
+	//region GetUsersCallback
 	override fun onSuccess(users: List<User>) {
 		this.users = users.map { presenter.format(it) }
 		view?.displayUsers(this.users)
