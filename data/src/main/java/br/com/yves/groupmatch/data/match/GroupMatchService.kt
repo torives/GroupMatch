3cc28cd22f844e1ec520650f5260de27ed2921fe
Victor Yves Crispim <rotcivyves@gmail.com>
2019-07-06 15:57:01 -0300

    Corrige URL do startMatch

--- data/src/main/java/br/com/yves/groupmatch/data/match/GroupMatchService.kt
+++ data/src/main/java/br/com/yves/groupmatch/data/match/GroupMatchService.kt
@@ -1,11 +1,12 @@
 package br.com.yves.groupmatch.data.match
 
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface GroupMatchService {
-    @POST("matches/{match_id}/lineup")
-    fun startMatch(match: RemoteMatch): Call<Response>
+    @POST("matches")
+    fun startMatch(@Body match: RemoteMatch): Call<Response>
 
     data class Response(
             var success: Boolean,
